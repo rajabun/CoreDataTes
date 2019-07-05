c1584b997e0990075fe17a716879e638337a2652
Muhammad Rajab <m.rajab.priharsanto@gmail.com>
2019-07-05 19:47:41 +0700

    Sudah selesai tahap 1

--- CoreDataTes/ViewController.swift
+++ CoreDataTes/ViewController.swift
@@ -7,14 +7,73 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var namaTF: UITextField!
+    @IBOutlet weak var phoneNumberTF: UITextField!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var phoneNumberLabel: UILabel!
+    
+    var contactData: [Contact]?
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        loadItems()
     }
-
-
+    
+    @IBAction func addButton(_ sender: Any)
+    {
+        let data = Contact(context: context)
+        data.nama = namaTF.text
+        data.phoneNumber = phoneNumberTF.text
+        
+        //save data to array contactData
+        contactData?.append(data)
+        saveItems()
+    }
+    
+    func saveItems()
+    {
+        do
+        {
+            try context.save()
+        }
+        catch
+        {
+            print("Error nih cuy", error)
+        }
+        
+        updateLayout()
+    }
+    
+    func loadItems()
+    {
+        let request : NSFetchRequest = Contact.fetchRequest()
+        
+        
+        do
+        {
+            contactData = try context.fetch(request)
+        }
+        catch
+        {
+            print("Error", error)
+        }
+        
+        
+    }
+    
+    func updateLayout()
+    {
+        nameLabel.text = contactData?.last?.nama
+        phoneNumberLabel.text = contactData?.last?.phoneNumber
+    }
 }
 
